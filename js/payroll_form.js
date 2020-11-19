909a74295ad7938a8d97aef807bb7143d3bd97a8
RohanBPatil <rohanpatil14218@gmail.com>
2020-11-19 22:52:30 +0530

    Added : Event listner on salary range

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -3,4 +3,4 @@ const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
-}); 
+});
