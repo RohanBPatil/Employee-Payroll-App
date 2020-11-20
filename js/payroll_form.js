898971cc537fbea21a8923b5db2a3920df2c1a74
RohanBPatil <rohanpatil14218@gmail.com>
2020-11-20 12:05:22 +0530

    Modified : Employee payroll class with new attributes and getters and setters

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -21,9 +21,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
             this._id = id;
         }
 
+        get profilePic() { return this._profilePic; }
+        set profilePic(profilePic) {
+            this._profilePic = profilePic;
+        }
+        get gender() { return this._gender; }
+        set gender(gender) {
+            this._gender = gender;
+        }
+
+        get department() { return this._department; }
+        set department(department) {
+            this._department = department;
+        }
+
         get salary() { return this._salary }
         set salary(salary) {
             this._salary = salary
+        }
+
+        get note() { return this._note; }
+        set note(note) {
+            this.note = note;
         }
 
         get startDate() { return this._startDate }
@@ -42,8 +61,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
             const options = { year: 'numeric', month: 'long', day: 'numeric' };
             const empDate = this.startDate === undefined ? "undefined" :
                 this.startDate.toLocaleDateString("en-US", options);
-            return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary +
-                ", start date = " + empDate;
+            return "id = " + this.id + ", name = " + this.name + ", gender=" + this.gender +
+                ", profilePic=" + this.profilePic + ", department=" + this.department + ", salary = "
+                + this.salary + ", start date = " + empDate + ", note=" + this.note;
         }
     }
 
